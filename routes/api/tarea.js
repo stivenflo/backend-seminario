fbda4379eb93300cf5d6862c4aa311035da0e688
Jorge_Stephen <43349540+stivenflo@users.noreply.github.com>
2021-03-31 13:52:28 -0600

    apis de tareas

--- routes/api/tarea.js
+++ routes/api/tarea.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-
 var mysql = require("mysql");
 
 var con = mysql.createPool({
@@ -11,3 +10,59 @@ var con = mysql.createPool({
     insecureAuth:true,
     multipleStatements:true
 });
+
+router.get('/get_tarea', (req, res, next) => {
+    var query = 'select * from tarea';
+    con.query(query, (err, result, field) => {
+       if(err){
+           next(err);
+       } else {
+           res.status(200).json(result)
+       }
+      });
+});
+
+router.post('/insert_tarea', (req, res, next) => {
+    var query = 'INSERT INTO tarea (titulo, tarea) values (?, ?)';
+    var values = [req.body.titulo,
+                  req.body.tarea];
+
+    con.query(query, values, (err, result, field) => {
+       if(err){
+           next(err);
+       } else {
+           res.status(200).json(result)
+       }
+      });
+});
+
+router.put('/update_tarea', (req, res, next) => {
+    var query = 'Update tarea set tarea=? WHERE titulo = ?';
+    
+    var values = [req.body.tarea,
+                  req.body.titulo];
+
+    con.query(query, values, (err, result, field) => {
+       if(err){
+           next(err);
+       } else {
+           res.status(200).json(result)
+       }
+      });
+});
+
+router.delete('/delete_tarea', (req, res, next) => {
+    var query = 'delete from tareas.tarea where titulo = ?';
+    
+    var values = [req.query.titulo];
+
+    con.query(query, values, (err, result, field) => {
+       if(err){
+           next(err);
+       } else {
+           res.status(200).json(result)
+       }
+      });
+});
+
+module.exports = router;
